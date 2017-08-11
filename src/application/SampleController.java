b6164460466ae959bd06c2897203dc957d330d4e
EssamEid614 <essameid614@gmail.com>
2017-08-11 19:15:55 +0200

    added ui stuff for login

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,11 +1,20 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class SampleController {
 
-	@FXML public void helloworld() {
-		System.out.println("hello world");
+	@FXML private TextField userName;
+	@FXML private PasswordField Password;
+	
+	@FXML public void Login()
+	{
+		String Name= userName.getText();
+		String Pass = Password.getText();
+		//send data to network
+		//retrieve whether a success login or failed
 	}
 	
 }
