595ecc712d47c3d23eea1ebe211fb2b6ee6b36f8
EssamEid614 <essameid614@gmail.com>
2017-08-12 00:06:12 +0200

    added server files as well this is the same as the previous commit , forgot to write this in msg

--- BankServer/src/main/Main.java
+++ BankServer/src/main/Main.java
@@ -20,7 +20,7 @@ public class Main {
 	        System.out.println(in.readUTF());
 	        System.out.println(in.readUTF());
 	        while(true);
-	        //ListenTo.close();
+	        ListenTo.close();
 		} 	
 		catch (IOException e) 
 		{
